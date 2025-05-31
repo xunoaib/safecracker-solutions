155186b8642fe9b6b069935a8ce65021ae90bf62
xunoaib <xunoaib@gmail.com>
2025-05-30 23:04:40 -0400

    Add cost function

--- rotating_symbol_tiles/rotating_symbol_tiles.py
+++ rotating_symbol_tiles/rotating_symbol_tiles.py
@@ -1,44 +1,59 @@
 #!/usr/bin/env python3
 
-import cv2
-
 ROWS = COLS = 5
 
 
-def parse_subimages(im):
-    '''Parses an images into subimages as a 5x5 list'''
+def dist(src, tar):
+    return abs(tar[0] - src[0]) + abs(tar[1] - src[1])
 
-    h, w, _ = im.shape
-    rowsize = h / ROWS
-    colsize = w / COLS
-    subimages = []
 
-    for r in range(ROWS):
-        row = []
-        for c in range(COLS):
-            ystart = int(r * rowsize)
-            yend = int(ystart + rowsize)
-            xstart = int(c * colsize)
-            xend = int(xstart + colsize)
-            subim = im[ystart:yend, xstart:xend]
-            print((xstart, ystart), (xend, yend))
-            if 0 in subim.shape:
-                print('Empty image')
-            row.append(subim)
-        subimages.append(row)
+def list_to_dict(grid):
+    return {
+        (r, c): v
+        for r, line in enumerate(grid)
+        for c, v in enumerate(line)
+    }
 
-    return subimages
+
+def find_num_pos(grid, num):
+    return next(
+        (
+            (r, c) for r in range(ROWS)
+            for c in range(COLS) if grid[r, c] == num
+        ), None
+    )
+
+
+def dist_to_solve(grid):
+    '''Sum of distances of tiles from their correct positions (NOT admissible)'''
+    cost = 0
+    for pos, num in grid.items():
+        goal_pos = find_num_pos(GOAL, num)
+        if goal_pos is not None:
+            cost += dist(pos, goal_pos)
+    return cost
 
 
 def main():
+    print('cost to solve:', dist_to_solve(INIT))
 
-    im = cv2.imread('2025-05-30_22-15_safecracker_tiles_cropped.png')
-    subimages = parse_subimages(im)
 
-    # for r, row in enumerate(subimages):
-    #     for c, subim in enumerate(row):
-    #         cv2.imwrite(f'tile_{r}_{c}.png', subim)
+# [23, 4, 14, 10, 17]
+# [8, 13, 24, 3, 9]
+# [0, 21, 2, 18, 11]
+# [19, 12, 22, 1, 7]
+# [6, 16, 5, 15, 20]
 
+INIT = list_to_dict([[r * COLS + c for c in range(COLS)] for r in range(ROWS)])
+GOAL = list_to_dict(
+    [
+        [23, 4, 14, -1, -1],
+        [8, 13, 24, 3, -1],
+        [0, 21, 2, 18, 11],
+        [-1, 12, 22, 1, 7],
+        [-1, -1, 5, 15, 20],
+    ]
+)
 
 if __name__ == '__main__':
     main()
