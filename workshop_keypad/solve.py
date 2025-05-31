d1da34735e5fc7afd6de494c49744ef038032e57
xunoaib <xunoaib@gmail.com>
2025-05-31 03:32:41 -0400

    Solve workshop keypad

--- workshop_keypad/solve.py
+++ workshop_keypad/solve.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from itertools import batched
 
 # The keypad buttons are malfunctioning, so they don't send the expected
 # numbers when pressed. Multiple presses of the same button will produce
@@ -6,6 +7,12 @@
 # it cycles through when pressed.
 
 # Correct combination: 5841
+
+
+def press(number, times=1):
+    for _ in range(times):
+        sequences[number] = sequences[number][1:] + sequences[number][:1]
+
 
 sequences = {
     1: [2, 7, 9, 4, 8],
@@ -18,3 +25,30 @@ sequences = {
     8: [9, 7, 6, 5, 4],
     9: [6, 8, 1, 3, 6],
 }
+
+combination = [5, 8, 4, 1]
+avail = set(sequences)
+solution = []
+final_seq = []
+
+# advance final combination buttons to the correct positions
+for c in combination:
+    num = min(
+        avail,
+        key=lambda v: sequences[v].index(c)
+        if c in sequences[v] else float('inf')
+    )
+    solution += [num] * sequences[num].index(c)
+    final_seq.append(num)
+    press(num, sequences[num].index(c))
+    avail.remove(num)
+
+# press other buttons for filler parity
+while rem := len(solution) % 4:
+    solution = [min(avail)] * rem + solution
+
+solution += final_seq
+
+for b in batched(solution, 4):
+    print(''.join(map(str, b)), end=' ')
+print()
