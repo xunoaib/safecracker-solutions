21fdc599d4a8942f942cdfc4908984ea39eb967d
xunoaib <xunoaib@gmail.com>
2025-06-01 16:02:05 -0400

    WIP digits

--- door_digits/solve.py
+++ door_digits/solve.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+# Safe contains 4 bars.
+# Pressing 6666, 7777, 8888, or 9999 causes one bar to temporarily retract.
+
 TEMPLATE = '''
   00  
  6  1 
@@ -43,7 +46,7 @@ def decode_segments(segment_strs):
 
 def display_segment(segment):
     out = TEMPLATE
-    for i, v in enumerate(f'{segment:07b}'):
+    for i, v in enumerate(f'{segment:07b}'[::-1]):
         out = out.replace(str(i), 'X' if v == '1' else '.')
     return out
 
@@ -57,16 +60,53 @@ def display(segment_or_segments):
     print()
 
 
+def merge(segments):
+    out = []
+    for g in zip(*segments):
+        r = 0
+        for v in g:
+            r |= v
+        out.append(r)
+    return out
+
+
+def subtract(orig_segments, segments_to_sub):
+    segments = []
+    for o, s in zip(orig_segments, segments_to_sub):
+        segments.append(o & (0b1111111 - s))
+    return segments
+
+
+def invert(segments):
+    return [0b1111111 - s for s in segments]
+
+
 NUM_DIGITS = 4
 
 segments = [0 for _ in range(NUM_DIGITS)]
 
 panel = decode_segments(['tr m b', 't br', 'tl tr br', 'tl m bl'])
-x = violet = decode_segments(['br', 'b', 't m', 'b tr'])
-x = yellow = decode_segments(['t m', 't', 'bl', 't m'])
-x = corridor = decode_segments(['tr', 'm', 'tr', 'bl br'])
+violet = decode_segments(['br', 'b', 't m', 'b tr'])
+yellow = decode_segments(['t m', 't', 'bl', 't m'])
+corridor = decode_segments(['tr', 'm', 'tr', 'bl br'])
 
-display(violet)
-display(panel)
-display(yellow)
-display(corridor)
+# display(violet)
+# display(panel)
+# display(yellow)
+# display(corridor)
+
+bathrooms = [violet, yellow, corridor]
+all = [panel, violet, yellow, corridor]
+
+v = merge(all)
+display(v)
+
+# display(invert(v))
+# display(merge([invert(v), panel]))
+
+# display(merge(bathrooms))
+exit(0)
+
+for p in bathrooms:
+    v = merge([panel, p])
+    display(v)
