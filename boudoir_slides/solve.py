add370b067e1499582a031985d6da63b82f38eeb
xunoaib <xunoaib@gmail.com>
2025-06-01 23:05:50 -0400

    WIP identify spots and neighbors

--- boudoir_slides/solve.py
+++ boudoir_slides/solve.py
@@ -14,12 +14,62 @@ start = '''
 ROWS, COLS = 6, 6
 
 
-@dataclass
+@dataclass(frozen=True)
 class Tile:
     id: str
     pos: tuple[int, int]
     orientation: Literal['v'] | Literal['h']
     length: int
+
+    # def __repr__(self):
+    #     return f'Tile(id={self.id} @ {self.pos})'
+
+    @property
+    def offset(self):
+        return (1, 0) if self.orientation == 'v' else (0, 1)
+
+    @property
+    def spots(self):
+        '''Returns all positions covered by this tile'''
+
+        return {
+            (
+                self.pos[0] + self.offset[0] * i,
+                self.pos[1] + self.offset[1] * i
+            )
+            for i in range(self.length)
+        }
+
+    @property
+    def positions_past_end(self):
+        '''Returns the adjacent positions immediately beyond either "end" of this piece'''
+
+        return [
+            (
+                self.pos[0] - self.offset[0],
+                self.pos[1] - self.offset[1],
+            ),
+            (
+                self.pos[0] + self.offset[0] * self.length,
+                self.pos[1] + self.offset[1] * self.length
+            )
+        ]
+
+    def neighbors(self):
+        '''Returns moved versions of this tile (in both possible directions).
+        Only returns moves which keep the tile within the grid boundaries.
+        '''
+
+        offsets = [
+            (self.offset[0], self.offset[1]),
+            (-self.offset[0], -self.offset[1])
+        ]
+
+        for roff, coff in offsets:
+            newpos = (self.pos[0] + roff, self.pos[1] + coff)
+            newtile = Tile(self.id, newpos, self.orientation, self.length)
+            if all(0 <= r < ROWS and 0 <= c < COLS for r, c in newtile.spots):
+                yield newtile
 
 
 def string_to_grid(start):
@@ -47,11 +97,30 @@ def parse_tiles(grid):
     ]
 
 
+def neighbors(all_tiles: list[Tile]):
+    '''Returns all valid neighboring states for this tile'''
+
+    for tile in all_tiles:
+        other_tiles = [t for t in all_tiles if t != tile]
+        for n in tile.neighbors():
+
+            # check for overlap
+            other_spots = {s for t in other_tiles for s in t.spots}
+            if not n.spots & other_spots:
+                yield frozenset(other_tiles + [n])
+
+
 def main():
     grid = string_to_grid(start)
-    tiles = parse_tiles(grid)
-    for t in tiles:
+    all_tiles = parse_tiles(grid)
+    for t in all_tiles:
         print(t)
+
+    print()
+    for n in neighbors(all_tiles):
+        print(n)
+
+    print(len(list(neighbors(all_tiles))))
 
 
 if __name__ == '__main__':
