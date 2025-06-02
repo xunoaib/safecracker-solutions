d92570f4cba7e9c93f7d426f35e6c7902b34738a
xunoaib <xunoaib@gmail.com>
2025-06-02 01:34:26 -0400

    Solve concentric circles

--- concentric_circles/solve.py
+++ concentric_circles/solve.py
@@ -1,4 +1,5 @@
-# ring values are specified clockwise starting from their rightmost arrow
+# Concentric Rings Puzzle
+# Ring values are specified clockwise starting from their rightmost arrow
 '''
 after rotating red cw:
     blue[1] = red[4]
@@ -19,8 +20,6 @@ after rotating blue cw:
     red[4] = blue[1]
 '''
 
-RED, GREEN, BLUE = range(3)
-
 RED_RING = 'g.b..r'
 GREEN_RING = '...g.b'
 BLUE_RING = '..gb.r'
@@ -32,6 +31,7 @@ def rotate_cw(state, ring_idx: int):
     result = list(map(list, state))
     result[ring_idx] = result[ring_idx][-1:] + result[ring_idx][:-1]
 
+    RED, GREEN, BLUE = range(3)
     others = {
         RED: (BLUE, GREEN),
         GREEN: (RED, BLUE),
@@ -50,10 +50,33 @@ def rotate_ccw(state, ring_idx: int):
     return state
 
 
+def solved(state):
+    return all(v[0] == v[-1] == c for c, v in zip('rgb', state))
+
+
+def neighbors(state):
+    for ring in range(3):
+        yield rotate_cw(state, ring), 'rgb'[ring] + ' cw'
+        yield rotate_ccw(state, ring), 'rgb'[ring] + ' ccw'
+
+
+def solve(state):
+    visited = {state}
+    q = [(state, tuple())]
+    while q:
+        state, path = q.pop(0)
+        if solved(state):
+            return path
+
+        for n, move in neighbors(state):
+            if n not in visited:
+                visited.add(n)
+                q.append((n, path + (move, )))
+
+
 def main():
-    state = START_STATE
-    print(state)
-    print(rotate_ccw(rotate_cw(state, 0), 0))
+    for move in solve(START_STATE):
+        print(move)
 
 
 if __name__ == '__main__':
