f7dc3fc589891115eee9ead1f91632a263203916
xunoaib <xunoaib@gmail.com>
2025-06-02 01:15:50 -0400

    Update other rings

--- concentric_circles/solve.py
+++ concentric_circles/solve.py
@@ -1,16 +1,47 @@
 # ring values are specified clockwise starting from their rightmost arrow
+'''
+after rotating red cw:
+    blue[1] = red[4]
+    green[2] = red[3]
+    blue[3] = red[2]
+    green[4] = red[1]
 
+after rotating green cw:
+    red[1] = green[4]
+    blue[2] = green[3]
+    red[3] = green[2]
+    blue[4] = green[1]
+
+after rotating blue cw:
+    green[1] = blue[4]
+    red[2] = blue[3]
+    green[3] = blue[2]
+    red[4] = blue[1]
+'''
+
+RED, GREEN, BLUE = range(3)
+
+RED_RING = 'g.b..r'
 GREEN_RING = '...g.b'
 BLUE_RING = '..gb.r'
-RED_RING = 'g.b..r'
 
 START_STATE = tuple(map(tuple, [RED_RING, GREEN_RING, BLUE_RING]))
 
 
 def rotate(state, ring_idx: int):
-    result = list(state)
-    result[ring_idx] = result[ring_idx][1:] + result[ring_idx][:1]
-    return tuple(result)
+    result = list(map(list, state))
+    result[ring_idx] = result[ring_idx][-1:] + result[ring_idx][:-1]
+
+    others = {
+        RED: (BLUE, GREEN),
+        GREEN: (RED, BLUE),
+        BLUE: (GREEN, RED),
+    }[ring_idx]
+
+    for i in range(4):
+        result[others[i % 2]][i + 1] = result[ring_idx][4 - i]
+
+    return tuple(map(tuple, result))
 
 
 def main():
