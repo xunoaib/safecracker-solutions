732eabc70049d79dafb605de23f354471042428b
xunoaib <xunoaib@gmail.com>
2025-06-03 04:02:08 -0400

    Gather tiles in each direction

--- loft_loop_pipes/solve.py
+++ loft_loop_pipes/solve.py
@@ -1,10 +1,21 @@
 import json
 from collections import defaultdict
 
-from z3 import And, If, Int, Or, Solver, sat
+from z3 import And, If, Int, Or, Solver
 
 DIR_INDEXES = 'N NE E SE S SW W NW'.split()
 N, NE, E, SE, S, SW, W, NW = range(len(DIR_INDEXES))
+
+DIR_OFFSETS = {
+    N: (-1, 0),
+    NE: (-1, 1),
+    E: (0, 1),
+    SE: (1, 1),
+    S: (1, 0),
+    SW: (1, -1),
+    W: (0, -1),
+    NW: (-1, -1),
+}
 
 
 def rotate(dir_indexes: set[int], offset: int):
@@ -12,20 +23,18 @@ def rotate(dir_indexes: set[int], offset: int):
     return {(d + offset) % len(DIR_INDEXES) for d in dir_indexes}
 
 
+def tile_in_direction(pos: tuple[int, int], direction: int):
+    roff, coff = DIR_OFFSETS[direction]
+    return pos[0] + roff, pos[1] + coff
+
+
 def main():
     with open('nodes.json') as f:
         nodes = json.load(f)
 
     tiles = defaultdict(set)
-    for (x, y), dir_idx in nodes:
-        tiles[x, y].add(DIR_INDEXES.index(dir_idx))
-
-    # print(tiles)
-    # s = tiles[0, 0]
-    # print(s)
-    # for _ in range(4):
-    #     s = rotate(s, -2)
-    #     print(s)
+    for (c, r), dir_idx in nodes:
+        tiles[r, c].add(DIR_INDEXES.index(dir_idx))
 
     rotations = {p: Int(f'o{i}') for i, p in enumerate(sorted(tiles))}
 
@@ -48,13 +57,21 @@ def main():
     # require that each pipe has a corresponding connection in the opposite tile.
     # for now, enforce only one constraint for a tile (UP)
 
-    p = (0, 4)
+    p = (4, 0)
     p_dirs = pd1, pd2 = connections[p]
 
-    q = (p[0], p[1] - 1)
+    q = (p[0] - 1, p[1])
     q_dirs = qd1, qd2 = connections[q]
 
-    s.add(If(pd1 == N, Or(qd1 == S, qd2 == S), True))
+    tiles_in_dirs = [tile_in_direction(p, d) for d in range(8)]
+
+    d = N
+    d_op = (d + 4) % 8
+    s.add(If(pd1 == d, Or(qd1 == d_op, qd2 == d_op), True))
+    s.add(If(pd2 == d, Or(qd1 == d_op, qd2 == d_op), True))
+
+    # s.add(If(pd1 == N, Or(qd1 == S, qd2 == S), True))
+    # s.add(If(pd2 == N, Or(qd1 == S, qd2 == S), True))
 
     print(connections[p], tiles[p])
     print(connections[q], tiles[q])
