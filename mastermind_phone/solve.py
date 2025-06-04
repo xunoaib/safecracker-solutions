879c7e7329350bac8c7499a0a2f85cd04d7c8170
xunoaib <xunoaib@gmail.com>
2025-06-04 17:03:23 -0400

    Add interactive guessing loop

--- mastermind_phone/solve.py
+++ mastermind_phone/solve.py
@@ -16,10 +16,7 @@ class Guesser:
     def __init__(self):
         self.responses = []
 
-    def add(
-        self, guess: tuple[int, int, int, int], response: tuple[int, int, int,
-                                                                int]
-    ):
+    def add(self, guess: tuple[int, ...], response: tuple[int, ...]):
         self.responses.append((guess, response))
 
     def candidates(self):
@@ -62,57 +59,71 @@ def init_solver(candidates):
     return solver, zdigits
 
 
-def interactive_z3(candidates):
-    '''Allows the user to manually enter their own guesses and feedback, using
-    z3 to narrow down possible codes'''
-
-    solver, zdigits = init_solver(candidates)
-    while True:
-        slns = find_all_solutions(solver, zdigits)
-        if len(slns) == 1:
-            print('Found solution: ', *slns[0], sep='')
-            break
-        elif len(slns) == 0:
-            print('No solution')
-            break
-
-        print('Current candidates:\n')
-        print(*map(format, slns))
-        guess = input('Guess? [xxxx] > ')
-        ncorrect = int(input('Number correct? > '))
-
-        add_guess_knowledge(solver, zdigits, guess, ncorrect)
-
-
-def automatic_z3(candidates):
-    '''Automatically suggests guesses, accepting feedback and using z3 to
+def interactive(candidates):
+    '''Allows the user to manually enter their own guesses and feedback to
     narrow down possible codes'''
 
-    solver, zdigits = init_solver(candidates)
-    print(zdigits)
+    guesser = Guesser()
     while True:
-        if len(slns) == 1:
-            print('\n\033[92;1mSolution: ', *slns[0], '\033[0m', sep='')
+        candidates = list(guesser.candidates())
+
+        print('Found', len(candidates), 'candidates')
+        if len(candidates) == 1:
+            print('Found solution: ', *candidates[0], sep='')
             break
-
-        elif len(slns) == 0:
-            print('\033[91;1mNo solution\033[0m')
+        elif len(candidates) == 0:
+            print('No solution')
             break
+        elif len(candidates) < 100:
+            print('Current candidates:\n')
+            print(*map(format, candidates))
 
-        guess = best_guess(slns)
+        print()
+        guess_str = input('\033[93mGuess? [xxxx] > \033[0m')
+        guess = tuple(map(int, guess_str))
+        response_str = input(
+            '\033[93mResponse? [wcp] (w)rong/(c)orrect/(p)artial > \033[0m'
+        ).lower()
+        response = tuple(
+            {
+                'w': WRONG,
+                'c': CORRECT,
+                'p': PARTIAL
+            }[v] for v in response_str
+        )
+        guesser.add(guess, response)
+        print()
 
-        print('Candidates:\n')
-        print('\033[38;5;248m' + ' '.join(map(format, slns)) + '\033[m')
-        print('\n\033[93;1mGuess: ', *guess, '\033[0m', sep='')
 
-        # We could read input from the user here, but for ease of use, we'll
-        # just simulate what the actual response would be.
-
-        # ncorrect = int(input('Number correct? > '))
-        ncorrect = feedback(guess)
-        print('\033[96mFeedback:', ncorrect, 'correct\033[0m')
-
-        add_guess_knowledge(solver, zdigits, guess, ncorrect)
+# def automatic_z3(candidates):
+#     '''Automatically suggests guesses, accepting feedback and using z3 to
+#     narrow down possible codes'''
+#
+#     solver, zdigits = init_solver(candidates)
+#     print(zdigits)
+#     while True:
+#         if len(slns) == 1:
+#             print('\n\033[92;1mSolution: ', *slns[0], '\033[0m', sep='')
+#             break
+#
+#         elif len(slns) == 0:
+#             print('\033[91;1mNo solution\033[0m')
+#             break
+#
+#         guess = best_guess(slns)
+#
+#         print('Candidates:\n')
+#         print('\033[38;5;248m' + ' '.join(map(format, slns)) + '\033[m')
+#         print('\n\033[93;1mGuess: ', *guess, '\033[0m', sep='')
+#
+#         # We could read input from the user here, but for ease of use, we'll
+#         # just simulate what the actual response would be.
+#
+#         # ncorrect = int(input('Number correct? > '))
+#         ncorrect = feedback(guess)
+#         print('\033[96mFeedback:', ncorrect, 'correct\033[0m')
+#
+#         add_guess_knowledge(solver, zdigits, guess, ncorrect)
 
 
 def format(digits):
@@ -124,8 +135,8 @@ def score_guess(guess, solution_candidates):
 
     buckets = defaultdict(list)
     for code in solution_candidates:
-        feedback = sum(g == c for g, c in zip(guess, code))
-        buckets[feedback].append(code)
+        response = sum(g == c for g, c in zip(guess, code))
+        buckets[response].append(code)
 
     # a good guess minimizes the size of the largest bucket (worst-case).
     # however, im not 100% sure feedback frequency is the best metric to use.
@@ -143,41 +154,20 @@ def best_guess(solution_candidates):
     return best
 
 
-def find_all_solutions(solver: Solver, zdigits):
-    solver.push()
-
-    solutions = []
-    while solver.check() == sat:
-        model = solver.model()
-        solution = [model[z].as_long() for z in zdigits]
-        solutions.append(solution)
-
-        block = []
-        for d in model.decls():
-            var = d()
-            val = model[d]
-            block.append(var != val)
-        solver.add(Or(block))
-
-    solver.pop()
-    return sorted(solutions)
-
-
 def main():
 
     candidates = [list(range(1, 10)) for _ in range(10)]
 
-    g = Guesser()
-    g.add((1, 2, 3, 4), (WRONG, WRONG, PARTIAL, CORRECT))
-    print(list(g.candidates()))
-    exit(0)
+    # g = Guesser()
+    # g.add((1, 2, 3, 4), (WRONG, WRONG, PARTIAL, CORRECT))
+    # print(list(g.candidates()))
+    # exit(0)
 
     if '-i' in sys.argv:
-        print('Candidates:', candidates, end='\n\n')
-        interactive_z3(candidates)
-    else:
-        print('Automatic solving:')
-        automatic_z3(candidates)
+        interactive(candidates)
+    # else:
+    #     print('Automatic solving:')
+    #     automatic_z3(candidates)
 
 
 if __name__ == '__main__':
