9abe383f609885e2032e708cd4aedf748b641301
xunoaib <xunoaib@gmail.com>
2025-06-04 17:38:14 -0400

    Add automatic solving

--- mastermind_phone/solve.py
+++ mastermind_phone/solve.py
@@ -11,6 +11,8 @@ from itertools import product
 
 WRONG, PARTIAL, CORRECT = range(3)
 
+ALL_POSSIBLE_CODES = list(product(range(1, 10), repeat=4))
+
 
 class Guesser:
     '''Accepts guesses and responses and generates possible candidates'''
@@ -26,7 +28,7 @@ class Guesser:
 
 
 def find_candidates(responses):
-    for candidate in product(range(1, 10), repeat=4):
+    for candidate in ALL_POSSIBLE_CODES:
         if all(
             feedback(guess, candidate) == response
             for guess, response in responses
@@ -48,7 +50,7 @@ def feedback(guess, solution):
     return result
 
 
-def interactive(candidates):
+def interactive():
     '''Allows the user to manually enter their own guesses and feedback to
     narrow down possible codes'''
 
@@ -80,68 +82,70 @@ def interactive(candidates):
         print()
 
 
-# def automatic_z3(candidates):
-#     '''Automatically suggests guesses, accepting feedback and using z3 to
-#     narrow down possible codes'''
-#
-#     solver, zdigits = init_solver(candidates)
-#     print(zdigits)
-#     while True:
-#         if len(slns) == 1:
-#             print('\n\033[92;1mSolution: ', *slns[0], '\033[0m', sep='')
-#             break
-#
-#         elif len(slns) == 0:
-#             print('\033[91;1mNo solution\033[0m')
-#             break
-#
-#         guess = best_guess(slns)
-#
-#         print('Candidates:\n')
-#         print('\033[38;5;248m' + ' '.join(map(format, slns)) + '\033[m')
-#         print('\n\033[93;1mGuess: ', *guess, '\033[0m', sep='')
-#
-#         # We could read input from the user here, but for ease of use, we'll
-#         # just simulate what the actual response would be.
-#
-#         # ncorrect = int(input('Number correct? > '))
-#         ncorrect = feedback(guess)
-#         print('\033[96mFeedback:', ncorrect, 'correct\033[0m')
-#
-#         add_guess_knowledge(solver, zdigits, guess, ncorrect)
+def automatic():
+    '''Allows the user to manually enter their own guesses and feedback to
+    narrow down possible codes'''
+
+    guesser = Guesser()
+    while True:
+        candidates = list(guesser.candidates())
+
+        print('Found', len(candidates), 'candidates')
+        if len(candidates) == 1:
+            print('Found solution: ', *candidates[0], sep='')
+            break
+        elif len(candidates) == 0:
+            print('No solution')
+            break
+        elif len(candidates) < 100:
+            print('Current candidates:\n')
+            print(*map(format, candidates))
+
+        print()
+
+        if len(candidates) == len(ALL_POSSIBLE_CODES):
+            guess = (1, 2, 3, 4)  # hardcode the first guess for time
+        else:
+            guess = best_guess(candidates)
+            assert guess is not None
+
+        print('Guess ', *guess, sep='')
+
+        response_str = input(
+            '\033[93mResponse? [wcp] (w)rong/(c)orrect/(p)artial > \033[0m'
+        ).lower()
+        response = tuple(map('wpc'.index, response_str))
+
+        guesser.add(guess, response)
+        print()
 
 
 def format(digits):
     return ''.join(str(d) for d in digits)
 
 
-def score_guess(guess, solution_candidates):
+def score_guess(guess, candidates):
     '''Returns a score indicating how well the guess splits the solution candidates'''
 
     buckets = defaultdict(list)
-    for code in solution_candidates:
-        response = sum(g == c for g, c in zip(guess, code))
-        buckets[response].append(code)
+    for solution in candidates:
+        response = feedback(guess, solution)
+        buckets[response].append(solution)
 
     # a good guess minimizes the size of the largest bucket (worst-case).
     # however, im not 100% sure feedback frequency is the best metric to use.
     return max(len(b) for b in buckets.values())
 
 
-def best_guess(solution_candidates):
-    min_score = float('inf')
-    best = None
-    for guess in product(range(1, 10), repeat=4):
-        score = score_guess(guess, solution_candidates)
-        if score < min_score:
-            min_score = score
-            best = guess
-    return best
+def best_guess(candidates):
+    best = (float('inf'), None)
+    for guess in ALL_POSSIBLE_CODES:
+        score = score_guess(guess, candidates)
+        best = min(best, (score, guess))
+    return best[1]
 
 
 def main():
-
-    candidates = [list(range(1, 10)) for _ in range(10)]
 
     # g = Guesser()
     # g.add((1, 2, 3, 4), (WRONG, WRONG, PARTIAL, CORRECT))
@@ -149,10 +153,9 @@ def main():
     # exit(0)
 
     if '-i' in sys.argv:
-        interactive(candidates)
-    # else:
-    #     print('Automatic solving:')
-    #     automatic_z3(candidates)
+        interactive()
+    else:
+        automatic()
 
 
 if __name__ == '__main__':
