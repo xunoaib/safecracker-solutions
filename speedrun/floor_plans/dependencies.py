5e4288e0cc0bf69e4352dab45374258890244845
xunoaib <xunoaib@gmail.com>
2025-06-08 06:10:00 -0400

    Update dependencies

--- speedrun/floor_plans/dependencies.py
+++ speedrun/floor_plans/dependencies.py
@@ -25,41 +25,66 @@ dependency_graph = defaultdict(set)
 
 # X requires Y
 REQS_TEXT = '''
-upstairs_wheels: steering_wheel
-4_pin_circuit: magnet_ball
-access_service_hall: study_keypad
-access_rest_of_basement: basement_knobs
-basement_water: piston
-triple_key_3: boudoir_slides
-polybius: carved_stone_block
-access_yellow_room: colored_wires
-colored_wires: special_pipe_key
-access_workshop: office_keypad
-access_fountain: double_doors
-lever: loft_queens
-library_keypad: triple_key_4
-loft_loop_pipes: carved_stone_block
-magnet_ball: 4_pin_circuit
-study_keypad: access_service_hall
-mastermind_phone: iron_key
-museum_square_numbers: access_library
-snooker: fountain_plug, lever
-rotating_symbol_tiles: l_eq_e_paper, access_triple_museum
-study_keypad: access_service_hall
-access_loft: upstairs_wheels
-access_boudoir: workshop_dials
-workshop_dials: gold_keycard
+upstairs_wheels: steering_wheel, access_second_floor
+polybius: carved_stone_block, access_second_floor
+colored_wires: special_pipe_key, access_second_floor
+loft_queens: lever, access_second_floor
+workshop_dials: gold_keycard, access_workshop
 music_box: access_violet_room
 door_digits: access_violet_room, triple_key_1, triple_key_2, triple_key_3, triple_key_4
+solve_triple_museum: access_triple_museum, museum_square_numbers, tile_elimination, currency_sudoku
+t_shaped_cabinet: t_shaped_key
+call_sarah: access_library
+library_keypad: access_library, screen_card_reader
+access_service_hall: study_keypad
+laser_safe: access_workshop
+workshop_keypad: access_workshop
+office_keypad: access_fountain, resistor, transistor, 8_pin_circuit, 4_pin_circuit
+study_keypad: red_magnetic_card, access_fountain
+basement_water: piston, access_rest_of_basement
+basement_wires: access_rest_of_basement
+concentric_circles: access_start
+mastermind_phone: access_second_floor
 
-# laser_safe: access_workshop
-# workshop_keypad: access_workshop
+# Room Access Conditions
+
+double_doors: access_start
+access_fountain: double_doors
+access_workshop: office_keypad, access_fountain
+access_service_hall: study_keypad, access_fountain
+access_yellow_room: colored_wires, access_second_floor
+access_triple_museum: rotating_symbol_tiles, access_fountain
+access_loft: upstairs_wheels, access_second_floor
+access_boudoir: workshop_dials
+access_violet_room: double_key, access_second_floor
+access_rest_of_basement: basement_knobs, access_service_hall
+access_library: solve_triple_museum, access_second_floor
+access_second_floor: brass_key
+
+# Puzzle Accessibility
+
+fountain: access_fountain, fountain_plug, piston
+caesar_wheel: access_fountain
+rotating_symbol_tiles: access_fountain
+directional_keypad: access_fountain
+magnet_ball: access_fountain
+
+kitchen_dumbwaiter: access_service_hall
+picture_swapping: access_service_hall
+basement_knobs: access_service_hall
+
+boudoir_slides: access_boudoir
+off_button: access_violet_room
 
 currency_sudoku: access_triple_museum
 tile_elimination: access_triple_museum
 museum_square_numbers: access_triple_museum
-access_violet_room: double_key
 
+driving: access_second_floor, gps_keycard
+snooker: access_second_floor, snooker_keycard
+
+loft_loop_pipes: access_loft
+loft_queens: access_loft
 '''
 
 # action => effects
@@ -67,15 +92,15 @@ REWARDS = {
     'double_doors': ['access_fountain'],
     'basement_wires': ['gps_keycard', 'brass_key'],
     'basement_knobs': ['access_rest_of_basement'],
-    'basement_water': ['piston'],
+    'basement_water': ['fountain'],
     'boudoir_slides': ['triple_key_3'],
     'caesar_wheel': ['8_pin_circuit', 'photo'],
     'mastermind_phone': ['gold_keycard', 'letter_from_margaret'],
     'colored_wires': ['access_yellow_room'],
     'call_sarah': ['steering_wheel'],
-    'off_button': ['magnetic_card', 'snooker_rules'],
+    'off_button': ['snooker_keycard', 'snooker_rules'],
     'concentric_circles': ['resistor', '4298_paper'],
-    'currency_sudoku': [],
+    # 'currency_sudoku': [],
     'directional_keypad': ['transistor'],
     'fountain': ['double_key'],
     'kitchen_dumbwaiter': ['special_pipe_key'],
@@ -84,18 +109,19 @@ REWARDS = {
     'loft_loop_pipes': ['carved_stone_block'],
     'loft_queens': ['triple_key_1'],
     'magnet_ball': ['4_pin_circuit'],
-    'museum_square_numbers': [],
+    # 'museum_square_numbers': [],
     'picture_swapping': ['laser_letter'],
     'polybius': ['triple_key_2'],
     'rotating_symbol_tiles': ['l_eq_e_paper', 'access_triple_museum'],
     'snooker': ['fountain_plug', 'lever'],
     'study_keypad': ['access_service_hall'],
-    'tile_elimination': [],
+    # 'tile_elimination': [],
     'upstairs_wheels': ['access_loft'],
     'workshop_dials': ['access_boudoir'],
     'workshop_keypad': ['red_magnetic_card'],
     'driving': ['6821_paper'],
-    'music_box': ['iron_key'],
+    'music_box': ['iron_key', 'screen_card_reader'],
+    't_shaped_cabinet': ['fountain_plug'],
 }
 
 # Goal => Requirements
